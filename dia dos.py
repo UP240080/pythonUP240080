85e5b734f519cc2c702e7bf8954ee3b228ec82ce
UP240080 <up240080@alumnos.upa.edu.mx>
2025-06-05 12:20:07 -0600

    Inicio del control de versiones

---
+++ dia dos.py
@@ -0,0 +1,40 @@
+"Dia 2:30 dias de programacion en python"
+Nombres= input("Escribe tu nombre")
+Apellidos= input("Escribe tus apellidos") 
+Nombres_Apellidos= Nombres+Apellidos
+print(Nombres_Apellidos)
+Pais=input("Escribe tu pais")
+Ciudad=input("Escribe tu ciudad")
+Edad= input("Escribe tu edad")
+Año=input("Escribe el año actual")
+Casado=input("Estas casado (si o no):")
+Verdadero=input("Escribe si es verdadero")
+Prendido=input("Esta la luz prendida (si o no)")
+A,B,C=5,10,15
+
+
+#nivel 2
+
+Pi=3.14
+print(len(Nombres))
+print(type(Pi))
+print("longitud de nombre:",len(Nombres))
+print("longitud del apellido:", len(Apellidos))
+num_uno, num_dos=5,4
+total= num_uno + num_dos
+resta= num_uno - num_dos
+multipicacion= num_uno * num_dos
+division= num_uno / num_dos
+porciento= num_dos % num_uno
+elevar= num_uno ** num_dos
+floor_division=num_uno // num_dos
+radio=30
+circumference= Pi * radio
+circuferencia= 2 * Pi * radio
+radio= float(input("ingresa el radio del circulo"))
+circulo= Pi* radio ** 2
+
+print("Nombre:", Nombres)
+print("Apellidos:", Apellidos)
+
+
---
+++ prueba.py
@@ -0,0 +1,29 @@
+print("Hola mundo")
+
+#día 1 - 30 días en Python
+print(5+2)
+print(3-6)
+print(12*3)
+print(6/2)
+print(8**2)
+print(15%2)
+print(5//2)
+
+#con este se miran los tipos de datos
+print(type(10))
+print(type(3.14))
+print(type(1+3j))
+print(type("Asabeneh"))
+print(type([1,2,3]))
+print(type({"name":"Asabeneh"}))
+print(type ({9.8,3.14,2.7}))
+print(type((9.8,3.14,2.7)))
+
+#Ejercicio
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3**4)
+print(3%4)
+print(3//4)
